d8a5c15777c5a6a359758cd7386d2917229c654d
MSI\h_mat <h_matsumoto@hotmail.com>
2022-10-17 17:35:30 +0900

    building 2D array vec; give-up

--- ModernCppChallenge/Ch02_17/main.cpp
+++ ModernCppChallenge/Ch02_17/main.cpp
@@ -13,10 +13,28 @@ using namespace std;
 スワップ
 */
 
+template <typename T>
+class array_iterator {
+    T & t;
+
+    array_iterator( T & t): t(t){}
+
+    typename T::reference operator *(){
+        return t[0];
+    }
+};
+
+
+
 template <class T, size_t R, size_t C>
 class Array{
+    using arrayCol = vector<T>;
+    using arrayVec = vector<arrayCol>;    
+
+    using reference = arrayCol &;
     // defining two-dimension vector with T type 
-    vector<vector <T>> arr;
+    arrayVec arr;
+
     public:
     // initializing R by C 2D-vector 
     Array(): Array(R, vector<T>(C)){};
@@ -41,17 +59,41 @@ class Array{
     vector<T> *data(){
         return arr.data();
     }
-};
 
+    const T& operator[](size_t i) {
+        if(0 <= i && i < R) return arr[i];
+        throw std::out_of_range("out of range");        
+    }
+
+    using iterator = array_iterator<Array>;
+    iterator begin(){
+        return iterator(*this);
+    }
+
+    iterator end();
+};
+ /*    move_iterator operator++(){
+        std::move_iterator temp = *this;
+        ++(*this);
+        return temp;
+    } */
 
 int main(){
-    Array<int, 2, 3> array {{1, 2, 3}, {4, 5, 6}};
-    
+    using TwoByThree = Array<int, 2, 3>;
+    TwoByThree array {{1, 2, 3}, {4, 5, 6}};
+    auto iter = array.begin();
+    cout << &iter << endl;
+    /*
     for(size_t i = 0; i < array.size(1); i++ ){
         for(size_t j = 0; j < array.size(2); j++ ){
             cout << i << ", " << j << ": " << array.at(i, j) << endl;
         }        
     }
-
-    return 0;
+    */
+   /*
+   for(auto itr = array.begin(); itr != array.end(); ++itr){
+    cout << *itr << endl;
+   }
+   */
+   return 0;
 }
