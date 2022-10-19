892181e2ce887338d26c0aaf540ff14edbbf722c
MSI\h_mat <h_matsumoto@hotmail.com>
2022-10-19 23:31:13 +0900

    可変引数テンプレート, 畳み込み式

--- ModernCppChallenge/Ch02_19/main.cpp
+++ ModernCppChallenge/Ch02_19/main.cpp
@@ -5,12 +5,18 @@
 任意個数の要素をコンテナに追加
 */
 
+template<typename C, typename... Args>
+void push_back(C& c, Args&&... args){
+  (c.push_back(args), ...); // 畳み込み式
+}
+
 int main(){
   std::vector<int> vec;
   vec.push_back(1);
 
   // task
-  // vec.push_back(1,2,3,4);
+  // push_back(vec,1,2,3,4);
+  push_back(vec,2,3,4,5);
 
   std::vector<int>::iterator itr = vec.begin();
   while(itr != vec.end()){
