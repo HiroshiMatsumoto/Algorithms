7c3d5faf6b4eb4d41338c50b8a3406448735c92a
MSI\h_mat <h_matsumoto@hotmail.com>
2022-10-22 01:05:14 +0900

    naive Burrow Wheeler and Decode are implemented

--- FastStringAnalysis/BurrowsWheeler/simple.py
+++ FastStringAnalysis/BurrowsWheeler/simple.py
@@ -3,24 +3,51 @@
 from SuffixArray.simple import suffix_array
 
 
-def burrows_wheeler(text: str) -> list:
+def rank(text):
+    return [text[:i + 1].count(text[i]) for i in range(len(text))]
+
+
+def burrows_wheeler_transform(text: str) -> list:
     """
     naive burrows wheeler implementation
     """
     tb = ["" for _ in range(len(text))]
     sa = suffix_array(text)
     for i in range(len(sa)):
-        if sa[i][1] == '\0':
-            tb[i] = text[-1 - 1]
-        elif sa[i][0] > 0:
+        if sa[i][0] > 0:
             tb[i] = text[sa[i][0] - 1]
+        elif sa[i][0] == 0:
+            tb[i] = text[-1]
     return tb
+
+
+def decode(tf, tb) -> str:
+    """
+    """
+    ranked_tb = list(zip(tb, rank(tb)))
+    ranked_tf = list(zip(tf, rank(tf)))
+    text = ""
+    index = ranked_tb.index(('\0', 1))
+    while ranked_tf[index] != ('\0', 1):
+        char = ranked_tf[index]
+        text += char[0]
+        index = ranked_tb.index(ranked_tf[index])
+    text += ranked_tf[index][0]
+    return text
 
 
 def main():
     text = "abracadabra\0"
-    tb = burrows_wheeler(text)
-    print(tb)
+
+    tb = burrows_wheeler_transform(text)
+
+    sa = suffix_array(text)
+    tf = [text[sa[i][0]] for i in range(len(text))]
+
+    decoded_text = decode(tf, tb)
+
+    assert text == decoded_text
+    print(decoded_text)
 
 
 if __name__ == "__main__":
