8710c639c6db513343905bf115bbc26ccb301b9e
MSI\h_mat <h_matsumoto@hotmail.com>
2022-10-21 23:56:41 +0900

    simple burrows wheeler transformation implementation

--- FastStringAnalysis/BurrowsWheeler/simple.py
+++ FastStringAnalysis/BurrowsWheeler/simple.py
@@ -3,22 +3,23 @@
 from SuffixArray.simple import suffix_array
 
 
-def burrows_wheeler(text):
+def burrows_wheeler(text: str) -> list:
+    """
+    naive burrows wheeler implementation
+    """
     tb = ["" for _ in range(len(text))]
     sa = suffix_array(text)
     for i in range(len(sa)):
         if sa[i][1] == '\0':
             tb[i] = text[-1 - 1]
-        elif sa[i][1] > 0:
-            tb[i] = text[ sa[i] - 1]
+        elif sa[i][0] > 0:
+            tb[i] = text[sa[i][0] - 1]
     return tb
 
 
 def main():
-    text = "hello world\0"
-    sa = suffix_array(text)
+    text = "abracadabra\0"
     tb = burrows_wheeler(text)
-    print(sa)
     print(tb)
 
 
